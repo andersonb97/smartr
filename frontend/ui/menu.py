b23548e4a72cb3a07cbdbb8e9b2a07c055639de2
Ben Anderson <andersonb97@gmail.com>
2025-06-18 13:19:26 -0500

    refactor to use bubble's client side webrtc

--- frontend/ui/menu.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def menu():
-    # Determine if a user is logged in or not, then show the correct
-    # navigation menu
-    st.set_option("client.showSidebarNavigation", False)
-    st.sidebar.page_link("pages/01_Interview.py", label="Interview")
